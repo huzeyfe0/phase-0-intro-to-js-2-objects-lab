f5fcddddd48d63b514c86ea72381366fff74e9f1
Huzeyfe <huzeyfe.abdullahoglu@bilgiedu.net>
2022-05-18 14:27:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ function updateEmployeeWithKeyAndValue(employee, key, value){
     return newObj;
 }
 
-
+//2
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
     employee[key] = value
     return employee
 }
-
+//3
 function deleteFromEmployeeByKey(employee, key){
     const newEmp = { ...employee }
     delete newEmp[key]
     return newEmp
 }
 
-
+//4git 
 function destructivelyDeleteFromEmployeeByKey(employee, key){
     delete employee[key]
     return employee
