a67414cf7f8c6a49f1f169e2cbfffd91eee3eb42
Huzeyfe <huzeyfe.abdullahoglu@bilgiedu.net>
2022-05-18 14:26:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Huzeyfe Abdullahoglu",
+    streetAdress: "istanbul, Kayasehir"
+}
+//1
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = { ...employee };
+    newObj[key] = value;
+    return newObj;
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmp = { ...employee }
+    delete newEmp[key]
+    return newEmp
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
